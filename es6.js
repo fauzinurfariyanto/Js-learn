290e5decd5a7499404dce2497b2983bdf72cb3f2
fauzinurfariyanto <fauzinur000@gmail.com>
2022-06-20 16:14:48 +0700

    Day 1

---
+++ es6.js
@@ -0,0 +1,19 @@
+/* Strings & Integers */
+
+// intro to javascript ES6
+console.log("Welcome to ES6 Study I!");
+console.log("Let's learn with Ken the Ninja!");
+
+// console.log() training \\
+
+// Print "Hello World" di console
+console.log('hello world');
+
+// Print "Belajar JS"
+console.log('Belajar JavaScript');
+
+// Number and Calculation
+
+
+
+
